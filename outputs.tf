1051fac387562d758966f78210e5302dda98ac42
oleksandr merzlikin <aleksander.merzlikin@nixsolutions.com>
2026-02-03 11:17:49 +0100

    fixing of namings in tf backend

--- outputs.tf
+++ outputs.tf
@@ -1,37 +1,26 @@
-# Outputs
+# Outputs for debugging and verification
+
+output "resource_prefix" {
+  description = "Resource prefix used"
+  value       = var.resource_prefix
+}
+
 output "resource_group_name" {
-  description = "Name of the resource group"
-  value       = azurerm_resource_group.main.name
+  description = "Resource group name"
+  value       = local.resource_group_name
 }
 
-output "virtual_network_name" {
-  description = "Name of the virtual network"
-  value       = azurerm_virtual_network.main.name
+output "common_tags" {
+  description = "Common tags applied to resources"
+  value       = local.common_tags
 }
 
-output "vm_public_ips" {
-  description = "Public IP addresses of the VMs"
-  value       = azurerm_public_ip.main[*].ip_address
+output "backend_config" {
+  description = "Backend configuration reference"
+  value       = local.backend_config
 }
 
-output "vm_names" {
-  description = "Names of the created VMs"
-  value       = azurerm_linux_virtual_machine.main[*].name
-}
-
-output "service_bus_namespace" {
-  description = "Service Bus namespace details"
-  value = {
-    name              = module.sbus.namespace_name
-    connection_string = module.sbus.primary_connection_string
-  }
-  sensitive = true
-}
-
-output "ssh_connection_commands" {
-  description = "SSH commands to connect to VMs"
-  value = [
-    for i, vm in azurerm_linux_virtual_machine.main :
-    "ssh adminuser@${azurerm_public_ip.main[i].ip_address}"
-  ]
+output "due_date" {
+  description = "Calculated due date"
+  value       = local.due_date
 }
